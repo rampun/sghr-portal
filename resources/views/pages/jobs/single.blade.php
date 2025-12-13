0d85e3948fa12c6fcdde951f57932f30f972effe
Ram Pun <rambdrpun@gmail.com>
2025-12-14 04:05:33 +0800

    minor change

--- resources/views/pages/jobs/single.blade.php
+++ resources/views/pages/jobs/single.blade.php
@@ -195,6 +195,12 @@
                             <p class="mb-2">{{ $recommendedJob->employer->company_name }}</p>
                             <div class="flex text-gray-600 items-center gap-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                </svg>
+                                <span>${{ number_format($job->salary_min_range)}}-${{ number_format($job->salary_max_range) }} (USD/month)</span>
+                            </div>
+                            <div class="flex text-gray-600 items-center gap-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
                                 </svg>
