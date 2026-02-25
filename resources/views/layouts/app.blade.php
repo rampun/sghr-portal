0e2b5eb69d5c13b80f6e8a9f4ec97b28da72c453
Ram Pun <rambdrpun@gmail.com>
2026-02-26 01:58:59 +0800

    clean up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
         @vite('resources/css/app.css')
 
          
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
         @filamentScripts
